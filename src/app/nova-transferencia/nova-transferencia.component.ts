6e55e9359afb38a1478e38de36ff52ec489a278e
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-12 08:29:51 -0300

    Estilizando o formulario

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,18 @@
-import { Component } from  '@angular/core';
+import { Component } from '@angular/core';
 
-@Component ({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
+export class novaTransferenciaComponet {
 
-export class novaTransferenciaComponet{
+  valor: number = 12;
+  destino: number = 222;
 
+  transferir() {
+    console.log('Solicitada nova transferência');
+    console.log('valor: ', this.valor);
+    console.log('destino: ', this.destino);
+  }
 }
