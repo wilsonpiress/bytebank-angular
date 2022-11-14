d696b97b440662b1fd73300b5c1cd163d3990be4
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-14 15:05:12 -0300

    Atualizações no código

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
+
 export class novaTransferenciaComponet {
 
-  valor: number = 12;
-  destino: number = 222;
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor!: number;
+  destino!: number;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('valor: ', this.valor);
-    console.log('destino: ', this.destino);
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
